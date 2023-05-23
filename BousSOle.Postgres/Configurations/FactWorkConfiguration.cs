d371e503a293d56a21be1c7887a7ed7d27f8e085
Krebkel <medvedev.andrew@mail.com>
2023-05-23 19:53:27 +0300

    Убран FactWorks, папка TrackingContracts расформирована за ненадобностью

--- BousSOle.Postgres/Configurations/FactWorkConfiguration.cs
+++ BousSOle.Postgres/Configurations/FactWorkConfiguration.cs
@@ -1,17 +1,16 @@
-using LSO.ProductivityContracts;
-using LSO.SSO.Reports;
+using LSO.SSO.ProductivityContracts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BousSOle.Postgres.Configurations;
 
-public class FactWorkEntityConfiguration : IEntityTypeConfiguration<FactWorkEntity>
+public class FactWorkEntityConfiguration : IEntityTypeConfiguration<FactWork>
 {
     /// <summary>
     /// Занесение экземпляра класса FactWorkEntity в таблицу FactWorkEntities с первичным ключом Id
     /// </summary>
     /// <param name="builder"></param>
-    public void Configure(EntityTypeBuilder<FactWorkEntity> builder)
+    public void Configure(EntityTypeBuilder<FactWork> builder)
     {
         builder.ToTable("FactWorkEntities");
         
